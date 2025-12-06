62053fbe1250cea43f3f9744cbba194caec49fcd
Johnathan31 <ay4718636@gmail.com>
2025-12-06 12:07:41 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+let equation = "";
+
+// Edit the equation vs display
+function appendToDisplay(input){
+  if (input == "*") {
+    display.value += "×";
+    equation += "*";
+  }
+  else if (input == "/") {
+    display.value += "÷"
+    equation += "/"
+  }
+  else if(input == "^"){
+    display.value += "^"
+    equation += "**"
+  }
+  else {
+    display.value += input;
+    equation += input;
+  }
+  
+}
+
+function ClearDisplay(){
+  display.value = "";
+  equation = "";
+}
+        
+function calculate(){
+  try{
+    display.value = eval(equation)
+    equation = display.value
+  }
+  catch(error){
+    display.value = "Error";
+    equation = "";
+  }
+}
+function special(){
+  display.value = display.value.slice(0, -1)
+  equation = equation.slice(0, -1)
+}
+  
